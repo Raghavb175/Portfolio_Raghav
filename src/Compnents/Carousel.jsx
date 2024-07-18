b1c5d392dc4ecaaa060008f846e4491630ad33be
Raghavb175 <raghavbhatia1705@outlook.com>
2024-07-17 23:23:26 -0600

    guj

--- src/Compnents/Carousel.jsx
+++ src/Compnents/Carousel.jsx
@@ -1,30 +1,12 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// import './CSS files/Carousel.css'; // Import CSS for styling
+// import React, { useState, useEffect, useRef, forwardRef } from 'react';
+// import './CSS files/Carousel.css'; // Keep this if you have custom styles; otherwise, it can be removed.
 
-// const Carousel = ({ slides, headerImage }) => {
+// const Carousel = forwardRef(({ slides, headerImage }, ref) => {
 //   const [currentSlide, setCurrentSlide] = useState(0);
-//   const [cardsToShow, setCardsToShow] = useState(1);
 //   const carouselRef = useRef(null);
 
-//   useEffect(() => {
-//     const handleResize = () => {
-//       if (carouselRef.current) {
-//         const containerWidth = carouselRef.current.offsetWidth;
-//         const cardWidth = 160; // Fixed width for each slide
-//         setCardsToShow(Math.floor(containerWidth / cardWidth));
-//       }
-//     };
-
-//     handleResize(); // Initial calculation
-//     window.addEventListener('resize', handleResize);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, []);
-
 //   const nextSlide = () => {
-//     if (currentSlide < slides.length - cardsToShow) {
+//     if (currentSlide < slides.length - 1) {
 //       setCurrentSlide(currentSlide + 1);
 //     }
 //   };
@@ -36,44 +18,52 @@
 //   };
 
 //   const calculateTranslateX = () => {
-//     return currentSlide * 200; // Translate by the width of one slide
+//     return currentSlide * 180; // Translate by the width of one slide
 //   };
 
 //   return (
-//     <div className='Carousel-main flex items-center justify-evenly max-h-fit -mb-12'>
-//       <div className='front-header text-purple-500 text-xl'>
-//         <img className='w-[65%] ml-7' src={headerImage} alt="Header" />
+//     <div ref={ref} className='Carousel-main flex items-center justify-between max-h-fit lg:justify-evenly sm:justify-start'>
+//       <div className='front-header text-purple-500 text-xl lg:w-60 mx-7'>
+//         <img className='w-[55%] ml-7 -mt-10 lg:w-[100%] sm:ml-0 m-0' src={headerImage} alt="Header" />
 //       </div>
-//       <div ref={carouselRef} className="carousel  mx-7 flex justify-start overflow-hidden">
+//       <div ref={carouselRef} className="carousel mx-5 flex justify-start overflow-hidden sm:mx-0 right-4">
 //         <div
-//           className="carousel-container flex transition-transform duration-500 "
+//           className="carousel-container flex transition-transform duration-500"
 //           style={{
 //             transform: `translateX(-${calculateTranslateX()}px)`,
 //             width: `${slides.length * 140}px` // Set the width based on the number of slides and fixed slide width
 //           }}
 //         >
 //           {slides.map((slide, index) => (
-//             <div key={index} className="carousel-slide mx-5" style={{ width: '140px' }}>
-//               <img src={slide.image} alt={slide.title} className="carousel-image w-full mx-auto" />
+//             <div key={index} className="carousel-slide mx-5 lg:mx-0 sm:-mx-4" style={{ width: '140px' }}>
+//               <img src={slide.image} alt={slide.title} className="carousel-image w-[70%] mx-auto" />
 //               <div className="carousel-content flex justify-center">
 //                 <h2>{slide.title}</h2>
 //               </div>
 //             </div>
 //           ))}
 //         </div>
-//         <button className="carousel-control prev" onClick={prevSlide}>
+//         <button
+//           className="carousel-control prev absolute top-1/2 transform -translate-y-1/2 left-[-25px] z-10"
+//           onClick={prevSlide}
+//         >
 //           &#10094;
 //         </button>
-//         <button className="carousel-control next" onClick={nextSlide}>
+//         <button
+//           className="carousel-control next absolute top-1/2 transform -translate-y-1/2 right-[-25px] z-10"
+//           onClick={nextSlide}
+//         >
 //           &#10095;
 //         </button>
 //       </div>
 //     </div>
-    
 //   );
-// };
+// });
 
 // export default Carousel;
+
+
+
 
 import React, { useState, useEffect, useRef, forwardRef } from 'react';
 import './CSS files/Carousel.css';
@@ -120,7 +110,7 @@ const Carousel = forwardRef(({ slides, headerImage }, ref) => {
   
     <div ref={ref} className='Carousel-main flex items-center justify-between max-h-fit max-lg:justify-evenly max-sm:justify-start'>
       
-      <div className='front-header text-purple-500 text-xl max-lg:w-60 mx-7'>
+      <div className='front-header text-purple-500 text-xl max-lg:w-60 mx-7 max-sm:-ml-2'>
         <img className='w-[55%] ml-7 -mt-10 max-lg:w-[100%] max-sm:ml-0 m-0' src={headerImage} alt="Header" />
       </div>
       <div ref={carouselRef} className="carousel  mx-5 flex justify-start overflow-hidden max-sm:mx-0 right-4">
@@ -129,6 +119,11 @@ const Carousel = forwardRef(({ slides, headerImage }, ref) => {
           style={{
             transform: `translateX(-${calculateTranslateX()}px)`,
             width: `${slides.length * 140}px` // Set the width based on the number of slides and fixed slide width
+          //  ` @media screen and (max-width: 992px) {
+          //     body {
+          //       background-color: blue;
+          //     }
+          //   }`
           }}
         >
           {slides.map((slide, index) => (
@@ -140,10 +135,10 @@ const Carousel = forwardRef(({ slides, headerImage }, ref) => {
             </div>
           ))}
         </div>
-        <button className="carousel-control prev" onClick={prevSlide}>
+        <button className="carousel-control prev max-sm:left-6" onClick={prevSlide}>
           &#10094;
         </button>
-        <button className="carousel-control next" onClick={nextSlide}>
+        <button className="carousel-control next max-sm:right-6" onClick={nextSlide}>
           &#10095;
         </button>
       </div>
